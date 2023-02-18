4d83a33a1574d5d760efc5045cd3d0d4f87a2c89
Zan <zan@protokol.com>
2023-02-18 18:23:18 +0100

    feat(cdk): Implement NFT Address Activity Initializer

---
+++ packages/cdk/lib/registers/AddressActivity/Initializer.ts
@@ -0,0 +1,24 @@
+import { Construct } from 'constructs';
+import { Trigger } from "aws-cdk-lib/triggers";
+import { AddressActivityConstruct, AddressActivityProps } from "./AddressActivity";
+
+export interface NFTActivityInitializerProps extends AddressActivityProps {
+
+}
+
+export class AddressActivityInitializer extends Construct {
+    public readonly addressActivityConstruct: AddressActivityConstruct;
+    public readonly trigger: Trigger;
+
+    constructor(scope: Construct, id: string, props: NFTActivityInitializerProps) {
+        super(scope, id);
+
+        this.addressActivityConstruct = new AddressActivityConstruct(scope, "AddressActivityInitializer", props);
+
+        this.trigger = new Trigger(this, "AddressActivityInitializerTrigger", {
+            handler: this.addressActivityConstruct.func,
+            executeAfter: [this],
+            executeOnHandlerChange: true,
+        });
+    }
+}
