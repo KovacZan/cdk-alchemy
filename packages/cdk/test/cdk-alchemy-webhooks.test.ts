6f10ad6a101af359f7dfbcc796626cf40d2f9a84
Zan <zan@protokol.com>
2023-02-18 15:24:19 +0100

    feat: Setup Monorepo

--- packages/cdk/test/cdk-alchemy-webhooks.test.ts
+++ packages/cdk/test/cdk-alchemy-webhooks.test.ts
@@ -2,7 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Template } from 'aws-cdk-lib/assertions';
 import { WebhookTrigger } from '../lib/index';
 
-test('SQS Queue Created', () => {
+test('CDK Alchemy webhooks', () => {
   const app = new cdk.App();
   const stack = new cdk.Stack(app, "TestStack");
 //   // WHEN
