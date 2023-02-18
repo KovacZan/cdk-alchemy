b6f86a325d1226ace51fd43d661faf196073f641
Zan <zan@protokol.com>
2023-02-18 18:27:12 +0100

    feat(cdk): Implement Mined Transactions Initializer

---
+++ packages/cdk/lib/registers/MinedTransactions/Initializer.ts
@@ -0,0 +1,24 @@
+import { Construct } from 'constructs';
+import { Trigger } from "aws-cdk-lib/triggers";
+import { MinedTransactionsConstruct, MinedTransactionsProps } from "./MinedTransactions";
+
+export interface MinedTransactionsInitializerProps extends MinedTransactionsProps {
+
+}
+
+export class MinedTransactionsInitializer extends Construct {
+    public readonly minedTransactionsConstruct: MinedTransactionsConstruct;
+    public readonly trigger: Trigger;
+
+    constructor(scope: Construct, id: string, props: MinedTransactionsInitializerProps) {
+        super(scope, id);
+
+        this.minedTransactionsConstruct = new MinedTransactionsConstruct(scope, "MinedTransactionsInitializer", props);
+
+        this.trigger = new Trigger(this, "MinedTransactionsInitializerTrigger", {
+            handler: this.minedTransactionsConstruct.func,
+            executeAfter: [this],
+            executeOnHandlerChange: true,
+        });
+    }
+}
