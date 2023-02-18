8d5d092a9df31d74e332e47632724548629328d9
Zan <zan@protokol.com>
2023-02-18 22:58:29 +0100

    refactor(cdk): Use Abstraction in NFT Activity

--- packages/cdk/lib/registers/NFTActivity/NFTActivity.lambda.ts
+++ packages/cdk/lib/registers/NFTActivity/NFTActivity.lambda.ts
@@ -1,44 +1,26 @@
-import { Alchemy, Network, WebhookType } from "alchemy-sdk";
-import { SecretsManager } from "@aws-sdk/client-secrets-manager";
+import { Network, WebhookType } from "alchemy-sdk";
 import { getSSMParameterByName } from "../../ssm-utils";
-import { isValidURL } from "../../utils";
+import { urlValidity } from "../../utils";
+import { checkNetworkValidity, initAlchemy } from "../../alchemy-utils";
+import { storeSecret } from "../../secrets-utils";
 
 const handler = async () => {
     const apiKey = process.env.ALCHEMY_API_KEY;
     const network = process.env.ALCHEMY_NETWORK as Network;
     const authToken = process.env.ALCHEMY_AUTH_TOKEN;
 
-    const destinationUrl = process.env.ALCHEMY_WEBHOOK_DESTINATION_URL;
+    const destinationUrl = process.env.ALCHEMY_WEBHOOK_DESTINATION_URL!;
     const contractAddress = process.env.ALCHEMY_CONTRACT_ADDRESS || ""; // TODO: create fake init
     const tokenId = process.env.ALCHEMY_TOKEN_ID || ""; // TODO: create fake init
 
-    if (!apiKey) {
-        throw new Error("API KEY not provided!");
-    }
-
-    if (!network) {
-        throw new Error("Network not provided!");
-    }
-    if (network !== Network.ETH_MAINNET && network !== Network.ETH_GOERLI) {
-        throw new Error("Network not supported");
-    }
-
-    if (!authToken) {
-        throw new Error("Auth Token not provided")
-    }
-
-    if (!destinationUrl) {
-        throw new Error("Webhook Destination URL not provided!")
-    }
-    if (!isValidURL(destinationUrl)) {
-        throw new Error("Invalid http or https Destination URL!")
-    }
-
-    const alchemyInstance = new Alchemy({
-        apiKey,
-        network,
-        authToken,
-    });
+    checkNetworkValidity(network, [
+        Network.ETH_MAINNET, Network.ETH_GOERLI,
+        Network.MATIC_MAINNET, Network.MATIC_MUMBAI,
+        Network.ARB_MAINNET, Network.ARB_GOERLI,
+        Network.OPT_MAINNET, Network.OPT_GOERLI,
+    ])
+    urlValidity(destinationUrl);
+    const alchemyInstance = initAlchemy(network, apiKey, authToken);
 
     const webhooks = await alchemyInstance.notify.getAllWebhooks()
     const isAlreadyCreated = webhooks.webhooks.some(webhook =>
@@ -59,17 +41,11 @@ const handler = async () => {
             ]
         });
 
-        const secret = {
-            WebhookId: response.id,
-            WebhookSigningKey: response.signingKey,
-        }
-
         const path = await getSSMParameterByName(process.env.SSM_PATH_TO_SECRET!);
 
-        const secretManager = new SecretsManager({});
-        await secretManager.createSecret({
-            Name: path,
-            SecretString: JSON.stringify(secret)
+        await storeSecret(path, {
+            WebhookId: response.id,
+            WebhookSigningKey: response.signingKey,
         })
     }
 
