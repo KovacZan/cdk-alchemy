63beb2a0e2ac07b09252d25929a2490bc14a814f
Zan <zan@protokol.com>
2023-02-21 22:34:39 +0100

    style(examples): Setup Eslint and Prettier

--- packages/examples/bin/examples.ts
+++ packages/examples/bin/examples.ts
@@ -1,18 +1,18 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { NFTActivityStack } from '../lib/Initializers/NFTActivityStack';
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { NFTActivityStack } from "../lib/Initializers/NFTActivityStack";
 import * as dotenv from "dotenv";
 
 dotenv.config();
 
 const app = new cdk.App();
 
-new NFTActivityStack(app, 'NFTActivityStack', {
-  env: {
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION
-  },
-  alchemyApiKey: process.env.ALCHEMY_API_KEY!,
-  alchemyAuthToken: process.env.ALCHEMY_AUTH_TOKEN!,
+new NFTActivityStack(app, "NFTActivityStack", {
+	env: {
+		account: process.env.CDK_DEFAULT_ACCOUNT,
+		region: process.env.CDK_DEFAULT_REGION
+	},
+	alchemyApiKey: process.env.ALCHEMY_API_KEY!,
+	alchemyAuthToken: process.env.ALCHEMY_AUTH_TOKEN!
 });
--- packages/examples/lib/Initializers/NFTActivityStack.ts
+++ packages/examples/lib/Initializers/NFTActivityStack.ts
@@ -1,23 +1,23 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
 import { NFTActivityInitializer } from "cdk-alchemy-webhooks";
 
 interface NFTActivityStackProps extends cdk.StackProps {
-  alchemyApiKey: string;
-  alchemyAuthToken: string;
+	alchemyApiKey: string;
+	alchemyAuthToken: string;
 }
 
 export class NFTActivityStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props: NFTActivityStackProps) {
-    super(scope, id, props);
+	constructor(scope: Construct, id: string, props: NFTActivityStackProps) {
+		super(scope, id, props);
 
-    new NFTActivityInitializer(this, "NFTActivityExample", {
-      alchemyApiKey: props.alchemyApiKey,
-      alchemyNetwork: "eth-mainnet",
-      alchemyAuthToken: props.alchemyAuthToken,
-      alchemyWebhookDestinationUrl: "https://www.google.com",
-      alchemyContractAddress: "0x026224A2940bFE258D0dbE947919B62fE321F042",
-      alchemyTokenId: "123"
-    })
-  }
+		new NFTActivityInitializer(this, "NFTActivityExample", {
+			alchemyApiKey: props.alchemyApiKey,
+			alchemyNetwork: "eth-mainnet",
+			alchemyAuthToken: props.alchemyAuthToken,
+			alchemyWebhookDestinationUrl: "https://www.google.com",
+			alchemyContractAddress: "0x026224A2940bFE258D0dbE947919B62fE321F042",
+			alchemyTokenId: "123"
+		});
+	}
 }
