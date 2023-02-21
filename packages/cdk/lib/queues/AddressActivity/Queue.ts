67b4d437a2867df71a2eb2402fe5aefeed562853
Zan <zan@protokol.com>
2023-02-21 20:41:43 +0100

    feat(cdk): Setup AddressActivity Queue

---
+++ packages/cdk/lib/queues/AddressActivity/Queue.ts
@@ -0,0 +1,17 @@
+import { Construct } from 'constructs';
+import { Queue, QueueProps } from "aws-cdk-lib/aws-sqs";
+
+export interface AddressActivityQueueProps {
+    queueProps: QueueProps
+}
+
+export class AddressActivityQueueConstruct extends Construct {
+
+    public readonly queue: Queue;
+
+    constructor(scope: Construct, id: string, private readonly props: AddressActivityQueueProps) {
+        super(scope, id);
+
+        this.queue = new Queue(this, "AddressActivityQueue", this.props.queueProps);
+    }
+}
