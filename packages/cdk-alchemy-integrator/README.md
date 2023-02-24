d1cab58b1ca5df34005ac71faedc0266f0585213
Zan <zan@protokol.com>
2023-02-24 17:36:22 +0100

    feat(cdk-alchemy-integrator): Package setup

---
+++ packages/cdk-alchemy-integrator/README.md
@@ -0,0 +1,11 @@
+# CDK Alchemy Integrator
+
+> :warning: **This package is still under development!**
+
+## Useful commands for development
+
+* `pnpm run build`   compile typescript to js
+* `pnpm run watch`   watch for changes and compile
+* `pnpm run test`    perform the jest unit tests
+* `pnpm run lint`    run eslint across this package
+* `pnpm run format`  apply prettier format
---
+++ packages/cdk-alchemy-integrator/test/cdk-alchemy-webhooks.test.ts
@@ -0,0 +1,16 @@
+import * as cdk from "aws-cdk-lib";
+import { Template } from "aws-cdk-lib/assertions";
+// import { WebhookTrigger } from '../lib/index';
+
+test("CDK Alchemy webhooks", () => {
+	const app = new cdk.App();
+	const stack = new cdk.Stack(app, "TestStack");
+	//   // WHEN
+	//   new WebhookTrigger.WebhookTriggerConstruct(stack, 'MyTestConstruct');
+	//   // THEN
+	//   const template = Template.fromStack(stack);
+	//
+	//   template.hasResourceProperties('AWS::Lambda::Function', {
+	//     Timeout: 900
+	//   });
+});
