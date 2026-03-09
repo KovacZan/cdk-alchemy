ec7f4cc506fdc5f481ce1876d936918d42f1a890
KovacZan <zan.kovi@proton.me>
2026-03-09 18:01:04 +0100

    chore(cdk-alchemy-webhooks): Remove AddressActivity and NFTActivity constructs and related files

--- packages/cdk-alchemy-webhooks/lib/index.ts
+++ packages/cdk-alchemy-webhooks/lib/index.ts
@@ -1,2 +1,1 @@
 export * from "./registers";
-export * from "./queues";
--- packages/cdk-alchemy-webhooks/lib/queues/index.ts
+++
@@ -1,2 +0,0 @@
-export { NFTActivityQueueConstruct as NFTActivityQueue } from "./NFTActivity";
-export { AddressActivityQueueConstruct as AddressActivityQueue } from "./AddressActivity";
